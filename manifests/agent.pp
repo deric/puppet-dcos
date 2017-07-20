d7a817954cbe8614f8b4fbae20582d456ae76518
Tomas Barton <barton.tomas@gmail.com>
2017-07-20 18:23:13 +0200

    support agent's attributes

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,12 +1,29 @@
 class dcos::agent (
   $public = false,
+  $attributes = {},
 ) inherits ::dcos {
-
 
   if $public {
     $dcos_mesos_service = 'dcos-mesos-slave'
   } else {
     $dcos_mesos_service = 'dcos-mesos-slave-public'
+  }
+
+  file {'/var/lib/dcos/mesos-slave-common':
+    ensure => 'present',
+    content => template('dcos/agent-common.erb'),
+  }
+
+  exec {'stop-dcos-agent':
+    command => "systemctl kill -s SIGUSR1 ${dcos_mesos_service} && systemctl stop ${dcos_mesos_service}",
+    path    => '/bin:/usr/bin:/usr/sbin',
+    notify  => Exec['dcos-systemd-reload'],
+  }
+
+  exec { 'dcos-systemd-reload':
+    command     => 'systemctl daemon-reload',
+    path        => '/bin:/usr/bin:/usr/sbin',
+    refreshonly => true,
   }
 
   service { $dcos_mesos_service:
