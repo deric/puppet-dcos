2bd9ff8e34fedf2de31f45b9ef998dcee8fe8f57
Tomas Barton <barton.tomas@gmail.com>
2017-07-20 18:47:23 +0200

    cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,24 +1,14 @@
 # Class: dcos
 # ===========================
 #
-# Full description of class dcos here.
+# Manages DC/OS nodes, ensures compatibility between distributions
 #
-# Parameters
-# ----------
-#
-# * `sample parameter`
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
+
 class dcos (
-  $package_name = $::dcos::params::package_name,
-  $service_name = $::dcos::params::service_name,
+
 ) inherits ::dcos::params {
 
-  # validate parameters here
-
   class { '::dcos::install': } ->
-  class { '::dcos::config': } ~>
-  class { '::dcos::service': } ->
+  class { '::dcos::config': } ->
   Class['::dcos']
 }
