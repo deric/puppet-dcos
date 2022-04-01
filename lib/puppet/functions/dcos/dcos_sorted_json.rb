47e11a7cd5ef513e8bf4fa13dd9a7bbefa6752ce
Tomas Barton <barton.tomas@gmail.com>
2022-04-01 16:45:03 +0200

    Update generated functions

--- lib/puppet/functions/dcos/dcos_sorted_json.rb
+++ lib/puppet/functions/dcos/dcos_sorted_json.rb
@@ -8,32 +8,6 @@
 # ---- original file header ----
 require 'json'
 
-def sorted_generate(obj)
-  case obj
-    when Array
-      arrayRet = []
-      obj.each do |a|
-        arrayRet.push(sorted_generate(a))
-      end
-      return "[" << arrayRet.join(',') << "]";
-    when Hash
-      ret = []
-      obj.keys.sort.each do |k|
-        ret.push(k.to_json << ":#{sorted_generate(obj[k])}")
-      end
-      return "{" << ret.join(",") << "}";
-    when String
-      return obj.to_json
-    else
-      return obj.to_json
-  end
-end # end def
-
-def sorted_json(h)
-  sorted_generate(h)
-end
-
-# ---- original file header ----
 #
 # @summary
 #   This function takes data, outputs making sure the hash keys are sorted
@@ -51,21 +25,38 @@ Puppet::Functions.create_function(:'dcos::dcos_sorted_json') do
   #   Describe what the function returns here
   #
   dispatch :default_impl do
-    # Call the method named 'default_impl' when this is matched
-    # Port this to match individual params for better type safety
-    repeated_param 'Any', :arguments
+    required_param 'Hash', :json
   end
 
+  def sorted_generate(obj)
+    case obj
+      when Array
+        arrayRet = []
+        obj.each do |a|
+          arrayRet.push(sorted_generate(a))
+        end
+        return "[" << arrayRet.join(',') << "]";
+      when Hash
+        ret = []
+        obj.keys.sort.each do |k|
+          ret.push(k.to_json << ":#{sorted_generate(obj[k])}")
+        end
+        return "{" << ret.join(",") << "}";
+      when String
+        return obj.to_json
+      else
+        return obj.to_json
+    end
+  end # end def
 
-  def default_impl(*arguments)
-    
+  def sorted_json(h)
+    sorted_generate(h)
+  end
 
-    raise(Puppet::ParseError, "sorted_json(): Wrong number of arguments " +
-      "given (#{arguments.size} for 1)") if arguments.size != 1
-
-    json = arguments[0]
+  def default_impl(json)
     return sorted_json(json)
-
-  
   end
+
+
+
 end
